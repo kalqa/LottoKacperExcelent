54d96c17576db3720857fe297a90c150acbc2819
golebiewskii.kacper@gmail.com <golebiewskii.kacper@gmail.com>
2022-10-24 18:26:56 +0200

    added WinningNumbersDto refactored methods in WinningNumbersGeneratorFacade

--- src/main/java/pl/lotto/numbergenerator/WinningNumbersGeneratorFacade.java
+++ src/main/java/pl/lotto/numbergenerator/WinningNumbersGeneratorFacade.java
@@ -1,24 +1,32 @@
 package pl.lotto.numbergenerator;
 
+import pl.lotto.numbergenerator.dto.WinningNumbersDto;
+
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
 public class WinningNumbersGeneratorFacade {
 
+    //TODO: Extract finals to config ??
     private final int LOWER_BAND = 1;
     private final int UPPER_BAND = 99;
 
     private final int RANDOM_NUMBER_BOUND = (UPPER_BAND-LOWER_BAND) +1;
 
-    public Set<Integer> generateWinningNumbers() {
+
+    public WinningNumbersDto generateWinningNumbers() {
         Set<Integer> winningNumbers = new HashSet<>();
         while(isAmountOfNumbersLowerThanSix(winningNumbers)) {
             Random random = new Random();
             int randomNumber = random.nextInt(RANDOM_NUMBER_BOUND);
             winningNumbers.add(randomNumber);
         }
-        return winningNumbers;
+        return WinningNumbersDto.builder()
+                .winningNumbers(winningNumbers)
+                .drawDate(LocalDate.now())
+                .build();
     }
 
     private static boolean isAmountOfNumbersLowerThanSix(Set<Integer> winningNumbers) {
---
+++ src/main/java/pl/lotto/numbergenerator/dto/WinningNumbersDto.java
@@ -0,0 +1,15 @@
+package pl.lotto.numbergenerator.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@Builder
+public class WinningNumbersDto {
+
+    private Set<Integer> winningNumbers;
+    private LocalDate drawDate;
+}
